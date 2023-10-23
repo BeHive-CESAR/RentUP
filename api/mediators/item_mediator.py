1841a23e212b7e9a5c709d4e52e9434ed8e914c4
Lucas-PC <lgbs@cesar.school>
2023-10-23 17:11:01 -0300

    tratamento de erros para itens inexistentes

--- api/mediators/item_mediator.py
+++ api/mediators/item_mediator.py
@@ -6,25 +6,33 @@ class ItemMediator:
         self.repo = ItensRepository()    
 
     def __validate_item(self, item:Item):
+        '''
+        Valida as quantidades de itens, caso esteja tudo OK, retorna False
+        '''
         if item.qntTotal < 0 or item.qntEstoque < 0 or item.qntEmprestar < 0 or item.qntEmprestados < 0 or item.qntDanificados < 0:
             return "Quantidade negativa de itens"
         elif item.qntTotal != item.qntEstoque+item.qntEmprestar+item.qntEmprestados+item.qntDanificados:
             return "Quantidade de itens total invalida"
         
-        return True
+        return False
     
     def create_item(self, item:Item):
         validacao = self.__validate_item(item)
-        if validacao:
-            self.repo.insert(item.to_banco())
+        if not validacao:
+            if self.get_item(item) == None:
+                self.repo.insert(item.to_banco())
+            else:
+                return "Item já existente"
         else:
             return validacao
     
     def edit_item(self, item1:Item, item2:Item):
         validacao = self.__validate_item(item2)
-        if validacao:
-            self.repo.update(item1.to_banco(), item2.to_banco())
-            return validacao
+        if not validacao:
+            if self.get_item(item1) != None:
+                self.repo.update(item1.to_banco(), item2.to_banco())
+            else:
+                return f"Item {item1.nome} não existe"
         else:
             return validacao
     
