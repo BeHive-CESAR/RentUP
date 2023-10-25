faffcf8ff892c2c1179a64f969a1ca201fa61675
Lucas-PC <lgbs@cesar.school>
2023-10-25 06:56:56 -0300

    Adicionando Docstrings para as classes Itens e User

--- infra/entities/itens.py
+++ infra/entities/itens.py
@@ -4,6 +4,7 @@ from infra.configs.base import Base
 from sqlalchemy import Column, String, Integer
 
 class Itens(Base):
+    '''Classe responsavel por espelhar um item da tabela itens no banco de dados'''
     __tablename__ = 'itens'
 
     nome_item = Column(String, primary_key=True)
@@ -15,4 +16,5 @@ class Itens(Base):
     descricao = Column(String, nullable=True)
 
     def __repr__(self):
+        '''Metodo mágico que é usado para representar o objeto como uma string que pode ser usada para criar um novo objeto com os mesmos valores.'''
         return f'[Item {self.nome_item}, Total {self.qnt_total}, Estoque {self.qnt_estoque}, Emprestados {self.qnt_emprestados}]'
--- infra/entities/users.py
+++ infra/entities/users.py
@@ -4,6 +4,7 @@ from infra.configs.base import Base
 from sqlalchemy import Column, String, Integer, ForeignKey, Boolean
 
 class User(Base):
+    '''Classe responsavel por espelhar um user da tabela users no banco de dados'''
     __tablename__ = 'users'
 
     nome = Column(String, nullable=False)
@@ -12,4 +13,5 @@ class User(Base):
     papel = Column(String, nullable=False)
 
     def __repr__(self):
+        '''Metodo mágico que é usado para representar o objeto como uma string que pode ser usada para criar um novo objeto com os mesmos valores.'''
         return f'[Nome: {self.nome}, email: {self.email}, role: {self.papel}]'
