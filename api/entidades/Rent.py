d8abff0ff9bd2cfdb8b42f5bf7856e97ff9ea357
Gabriel-Chaves0 <g.chaves0110@gmail.com>
2023-10-26 21:55:43 -0300

    Criação dos metodos do rent_mediator

--- api/entidades/Rent.py
+++ api/entidades/Rent.py
@@ -1,26 +1,26 @@
 from pydantic import BaseModel
 from .Item import Item
-from .Role import Role
 from datetime import datetime
 from .Users import Users
 from .Status import Status
+from typing import Optional
 
 class Rent(BaseModel):
     user: Users
     itens: Item
     rentDate: datetime
-    returnDate: datetime
+    returnDate: Optional[datetime]=None
     status: Status
 
     def to_banco(self):
         '''Metodo que tranforma o objeto Rent em um rent para o banco de dados'''
         data_insert = Rent(
-                    user=self.user, # Como puxar o user itens e status
-                    itens=self.itens,
-                    rentdate=self.rentDate,
-                    returndate=self.returnDate,
-                    status=self.status.name,
-
+                    user_email=self.user, 
+                    item_nome=self.itens,
+                    data_emprestimo=self.rentDate,
+                    data_devolucao=self.returnDate,
+                    estado=self.status.name,
+                    
                 )
         return data_insert
     
--- api/mediators/rent_mediator.py
+++ api/mediators/rent_mediator.py
@@ -1,22 +1,73 @@
 from api.entidades.Rent import Rent
 from infra.repository.rent_repository import RentRepository
+from infra.repository.itens_repository import ItensRepository, Itens
+from api.entidades.Users import User
+from .item_mediator import ItemMediator, Item
+from datetime import datetime
 
 class RentMediator:
+    def __init__(self):
+        self.repo=RentRepository()
+        self.repoItens=ItensRepository()
+        self.medItens=ItemMediator()
+
+    def __validate(self, rent:Rent):
+        ''' Metodo reponsável por validar se o Rent pode ser realizado
+
+        Keywords arguments:
+
+        rent -- variavel do tipo Rent que será validada
+        '''
+        item=Item(nome=rent.itens)
+        item_atual=self.medItens.get_item(item)
+        
+        if item_atual!=None:
+            if item_atual.qnt_emprestar>0:
+                return item_atual
+            else:
+                return "Item indisponível para empréstimo."
+        else:
+            return "Item inexistente."
     
-    def validate_rent():
-        print("inserir codigo")
+    def rentup_item(self, rent:Rent):
+        ''' Metodo reponsável por criar um novo emprestimo no banco de dados
 
-    def rentup_item(Rent):
-        print("inserir codigo")
+        Keywords arguments:
 
-    def return_item(Rent):
-        print("inserir codigo")
+        rent -- variavel do tipo Rent que será adicionada ao banco
+        '''
+        item=self.__validate(rent)
+        
+        if type(item)==Itens:
+            self.repo.insert(rent.to_banco())
+            self.repoItens.update_rent(item)
+        else:
+            return item
 
-    def get_history():
-        print("inserir codigo")
+    def return_item(self, rent:Rent):
+        ''' Metodo reponsável por retornar um emprestimo a coluna de emprestar e adiciona a data de devolução
 
-    def get_history_by_item(Item):
-        print("inserir codigo")
+        Keywords arguments:
 
-    def get_history_by_user(User):
-        print("inserir codigo")
+        rent -- variavel do tipo Rent que será retornada
+        '''
+        item=Item(nome=rent.itens)
+        item_atual=self.medItens.get_item(item)
+
+        rent2=rent
+        rent2.returnDate=datetime.now()
+
+        self.repo.update(rent, rent2)
+        self.repoItens.update_return(item_atual)
+
+    def get_history(self):
+        '''Retorna o historico de empréstimos'''
+        return self.repo.select()
+
+    def get_history_by_item(self, item:Item):
+        '''Retorna o historico de empréstimos por item'''
+        return self.repo.select_by_item(item)
+
+    def get_history_by_user(self, user:User):
+        '''Retorna o historico de empréstimos por usuario'''
+        return self.repo.select_by_user(user)
