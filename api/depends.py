12ec8ec3fe5df84a55568280bd84022d82fca30c
Lucas-PC <lgbs@cesar.school>
2023-11-06 06:57:00 -0300

    autenticação funcionando, porem desorganizada

--- api/depends.py
+++ api/depends.py
@@ -1,20 +1,8 @@
-from fastapi import Depends
-from fastapi.security import OAuth2PasswordBearer
-from sqlalchemy.orm import Session
+from fastapi import Security
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from api.mediators.user_mediator import UserMediator
 
-oauth_scheme = OAuth2PasswordBearer(tokenUrl='/user/login')
 
-
-def get_db_session():
-    try:
-        session = Session()
-        yield session
-    finally:
-        session.close()
-
-
-def token_verifier(token = Depends(oauth_scheme)):
-    uc = UserMediator()
-    uc.verify_token(acess_token=token)
+def auth_wrapper(auth: HTTPAuthorizationCredentials = Security(HTTPBearer())):
+    return UserMediator().verify_token(auth.credentials)
     
--- api/rotas.py
+++ api/rotas.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, Depends
 from .controllers.ItemController import ItemController as Item
 from .controllers.UsersController import UsersController as User
-from .depends import token_verifier
+from .depends import auth_wrapper
 
 router = APIRouter()
 
-router.include_router(Item().router, prefix="/item", dependencies=[Depends(token_verifier)])
+router.include_router(Item().router, prefix="/item", dependencies=[Depends(auth_wrapper)])
 router.include_router(User().router, prefix="/user")
 
