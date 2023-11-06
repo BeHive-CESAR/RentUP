6b76c206b77b9cba92d64ae80b27ee47650c68d3
Lucas-PC <lgbs@cesar.school>
2023-11-06 08:42:05 -0300

    função create all tables

--- api/main.py
+++ api/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from api.rotas import router
+from infra.configs.connection import DBConnectionHandler
 
 app = FastAPI()
+
+with DBConnectionHandler() as db:
+    db.create_all_tables()
 
 @app.get('/')
 async def root():
