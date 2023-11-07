0bf46b1219fb170f696a5416292d75b4f1ceff18
Lucas-PC <lgbs@cesar.school>
2023-11-07 16:06:59 -0300

    Endpoints useres

--- api/controllers/UsersController.py
+++ api/controllers/UsersController.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, status, Depends
 from fastapi.responses import JSONResponse
 from api.mediators.user_mediator import UserMediator, Users, UserAuth
 from api.depends import auth_wrapper
+import urllib.parse
 
 
 class UsersController:
@@ -28,17 +29,20 @@ class UsersController:
         @self.router.get('/get-users')
         async def get_users(token_verify=Depends(auth_wrapper)):
             users_list = UserMediator().get_users()
+            user_data = [{'nome': user.nome, 'email': user.email, 'contato': user.contato, 'role': user.papel} for user in users_list]
             return JSONResponse(
-                content=users_list,
+                content=user_data,
                 status_code=status.HTTP_200_OK
             )
             
 
-        @self.router.get('/get-by-user')
-        async def get_by_user(nome:str ,token_verify=Depends(auth_wrapper)):
-            user_names = UserMediator().get_user_by_name(nome)
+        @self.router.get('/get-user-by-name')
+        async def get_user_by_name(nome:str ,token_verify=Depends(auth_wrapper)):
+            nome = urllib.parse.unquote(nome)
+            users_list = UserMediator().get_user_by_name(nome)
+            user_data = [{'nome': user.nome, 'email': user.email, 'contato': user.contato, 'role': user.papel} for user in users_list]
             return JSONResponse(
-                content= user_names,
+                content= user_data,
                 status_code=status.HTTP_200_OK
             )
 
@@ -51,8 +55,8 @@ class UsersController:
             )
 
         @self.router.delete('/delete-user')
-        async def user_delete(user: Users, token_verify=Depends(auth_wrapper)):
-            UserMediator().delete_user(user)
+        async def user_delete(email:str, token_verify=Depends(auth_wrapper)):
+            UserMediator().delete_user(email)
             return JSONResponse(
                 content={'Deletado': 'Sucesso'},
                 status_code=status.HTTP_201_CREATED
