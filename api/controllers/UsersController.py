cd1ba88986f618d9be7b0a44062e71a332669b30
Lucas-PC <lgbs@cesar.school>
2023-11-08 10:48:50 -0300

    adicionando docstrings aos endpoints

--- api/controllers/UsersController.py
+++ api/controllers/UsersController.py
@@ -11,12 +11,45 @@ class UsersController:
 
         @self.router.post('/register')
         async def user_register(user: Users):
-            user_on_db = UserMediator().create_user(user)
-            if user_on_db:
-                return HTTPException(
-                    detail={'Erro': user_on_db},
-                    status_code=status.HTTP_400_BAD_REQUEST
-                )
+            '''
+            ### Endpoint de Registro de Usuário
+
+            Registra um novo usuário no sistema com os seguintes detalhes:
+
+            **Endpoint:** `POST /user/register`
+
+            **Parâmetros da Requisição:**
+            - **email** (string): O endereço de e-mail do usuário.
+            - **password** (string): A senha do usuário.
+            - **nome** (string): O nome completo do usuário.
+            - **contato** (string): O número de telefone do usuário.
+            - **cargo** (string): O cargo ou função do usuário.
+
+            **Códigos de Resposta:**
+            - **201 Created**: O registro do usuário foi bem-sucedido. O usuário foi criado com sucesso.
+            - **400 Bad Request**: A solicitação de registro não atende aos requisitos (por exemplo, campos em branco, formato de e-mail inválido).
+            - **409 Conflict**: Conflito de dados. O endereço de e-mail já está em uso por outro usuário.
+
+            **Exemplo de Uso:**
+            ```python
+            import requests
+
+            data = {
+                "email": "novo_usuario@example.com",
+                "password": "senha_segura",
+                "nome": "Novo Usuário",
+                "contato": "+1234567890",
+                "cargo": "USER"
+            }
+
+            response = requests.post("https://exemplo.com/user/register", data=data)
+            if response.status_code == 201:
+                print("Novo usuário registrado com sucesso.")
+            else:
+                print("Falha no registro. Verifique os dados fornecidos ou o endereço de e-mail já está em uso.")
+
+            '''
+            UserMediator().create_user(user)
             return JSONResponse(
                 content={'Registro': 'Sucesso'},
                 status_code=status.HTTP_201_CREATED
@@ -24,28 +57,124 @@ class UsersController:
         
         @self.router.post('/login')
         async def user_login(user: UserAuth):
+            '''
+            ### Endpoint de Login
+
+            Autentica um usuário no sistema por meio de email e senha.
+
+            **Endpoint:** `POST /user/login`
+
+            **Parâmetros da Requisição:**
+            - **email** (string): O endereço de e-mail do usuário.
+            - **password** (string): A senha do usuário.
+
+            **Códigos de Resposta:**
+            - **200 OK**: A autenticação foi bem-sucedida. O usuário está autenticado e recebe um token de acesso.
+            - **401 Unauthorized**: Falha na autenticação. As credenciais fornecidas são inválidas.
+
+            **Exemplo de Uso:**
+            ```python
+            import requests
+
+            data = {
+                "email": "usuario@example.com",
+                "password": "senha_segura"
+            }
+
+            response = requests.post("https://rentup.com/user/login", data=data)
+            if response.status_code == 200:
+                token = response.json()["token"]
+                print(f"Usuário autenticado com sucesso. Token de acesso: {token}")
+            else:
+                print("Falha na autenticação. Verifique suas credenciais.")
+
+            '''
 
             auth_data = UserMediator().user_login(user=user)
             return JSONResponse(
                 content=auth_data,
-                status_code=status.HTTP_202_ACCEPTED
+                status_code=status.HTTP_200_OK
             )
         
         @self.router.get('/get-users')
         async def get_users(token_verify=Depends(auth_wrapper)):
+            """
+            ### Lista de Usuários Cadastrados
+
+            Retorna uma lista de todos os usuários cadastrados no sistema.
+
+            **Endpoint:** `GET /user/get-users`
+
+            **Parâmetros da Requisição:**
+            - Nenhum.
+
+            **Códigos de Resposta:**
+            - **200 OK**: A solicitação foi bem-sucedida. Retorna uma lista de todos os usuários cadastrados.
+            - **404 Not Found**: Não foram encontrados usuários no sistema.
+
+            **Exemplo de Uso:**
+            ```python
+            import requests
+
+            response = requests.get("https://rentup.com/user/get-users")
+            if response.status_code == 200:
+                usuarios = response.json()
+                for usuario in usuarios:
+                    print(usuario)
+            else:
+                print("Nenhum usuário encontrado no sistema.")
+
+            """
             users_list = UserMediator().get_users()
             user_data = [{'nome': user.nome, 'email': user.email, 'contato': user.contato, 'role': user.papel} for user in users_list]
+            if len(users_list) == 0:
+                return JSONResponse(
+                content=user_data,
+                status_code=status.HTTP_404_NOT_FOUND
+                )
             return JSONResponse(
                 content=user_data,
                 status_code=status.HTTP_200_OK
-            )
+                )
             
 
         @self.router.get('/get-user-by-name')
         async def get_user_by_name(nome:str ,token_verify=Depends(auth_wrapper)):
+            '''
+            ### Consultar Usuário por Nome
+
+            Recupera informações de um usuário específico com base no seu nome.
+
+            **Endpoint:** `GET /api/get-user-by-name?nome={nome}`
+
+            **Parâmetros da Requisição:**
+            - **nome** (string): O nome do usuário que deseja consultar.
+
+            **Códigos de Resposta:**
+            - **200 OK**: A solicitação foi bem-sucedida. Retorna informações do usuário com o nome correspondente.
+            - **404 Not Found**: Nenhum usuário com o nome especificado foi encontrado no sistema.
+
+            **Exemplo de Uso:**
+            ```python
+            import requests
+
+            nome_do_usuario = "NomeUsuario"
+
+            response = requests.get(f"https://exemplo.com/api/get-user-by-name?nome={nome_do_usuario}")
+            if response.status_code == 200:
+                usuario = response.json()
+                print(f"Informações do usuário {nome_do_usuario}:{usuario}")
+            else:
+                print(f"Usuário com o nome {nome_do_usuario} não encontrado no sistema.")
+            '''
             nome = urllib.parse.unquote(nome)
             users_list = UserMediator().get_user_by_name(nome)
             user_data = [{'nome': user.nome, 'email': user.email, 'contato': user.contato, 'role': user.papel} for user in users_list]
+            if len(users_list) == 0:
+                return JSONResponse(
+                content=user_data,
+                status_code=status.HTTP_404_NOT_FOUND
+                )
             return JSONResponse(
                 content= user_data,
                 status_code=status.HTTP_200_OK
